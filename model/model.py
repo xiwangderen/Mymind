78cc3f912f644b64d61bc5da3b42143bafa37fdd
xiwangderen <2263097228@qq.com>
2026-09-22 16:49:43 +0800

    FFN

--- model/model.py
+++ model/model.py
@@ -1,6 +1,5 @@
 from transformers import PretrainedConfig
 
-
 class MymindConfig(PretrainedConfig):
     model_type = "mokiomind"
 
@@ -295,3 +294,21 @@ class Attention(nn.Module):
         output = output.transpose(1, 2).reshape(bsz, seq_len, -1)
         output = self.resid_dropout(self.o_proj(output))
         return output, past_kv
+
+from transformers.activations import ACT2FN
+class FeedForward(nn.Module):
+    def __init__(self,  config:MymindConfig):
+        super().__init__()
+        if config.intermediate_size is None:
+            # 计算合适的中间层维度 64的整数倍
+            intermediate_size = int(config.hidden_size * 8 / 3)
+            config.intermediate_size = 64 * ((intermediate_size + 64 - 1) // 64)
+        self.gate_proj = nn.Linear(config.hidden_size, config.intermediate_size, bias=False)
+        self.down_proj = nn.Linear( config.intermediate_size, config.hidden_size, bias=False)
+        self.up_proj = nn.Linear(config.hidden_size, config.intermediate_size, bias=False)
+        self.dropout = nn.Dropout(config.dropout)
+        self.act_fn = ACT2FN[config.hidden_act]
+
+    def forward(self, x):
+        return self.dropout(self.down_proj(self.up_proj(x) * self.act_fn(self.gate_proj(x))))
+
